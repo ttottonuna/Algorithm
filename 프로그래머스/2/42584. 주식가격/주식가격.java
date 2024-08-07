d464e68792d0f4bd328e7524e8777f15999197c0
Angela <wpfkf4644@gmail.com>
2024-08-08 00:02:46 +0900

    [level 2] Title: 주식가격, Time: 31.41 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,26 +1,28 @@
-import java.util.Deque;
-import java.util.ArrayDeque;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length] ;
+        //1. 리턴 배열로 만들고, 스택 만들기
+        int[] answer = new int[prices.length];
         Deque<Integer> stack = new ArrayDeque<>();
         
-        for(int i = 0; i<prices.length; i++) {
+        //2. 리턴값 규칙 만들기  여기서 i는 인덱스 :0부터 시작한당
+        for(int i =0; i <prices.length; i++ ) {
             while(!stack.isEmpty()) {
                 int j = stack.peek();
-                
-                if(prices[j]>prices[i]) {
-                    answer[j] = i -j;
+                if(prices[j] > prices[i]) {
+                    answer[j] = i - j;
                     stack.pop();
-                } else break;
+                } else 
+                    break;
             }
-               stack.push(i); 
+            stack.push(i);    
         }
-        while(!stack.isEmpty()) {
-            int i = stack.pop();
-            answer[i] = prices.length - i -1;
+       //3. 나머지 안떨어진 값들 구하기
+            while(!stack.isEmpty()) {
+                int i = stack.pop();
+             answer[i] = prices.length-1-i;
         }
-    return answer;
+        return answer;
     }
 }
