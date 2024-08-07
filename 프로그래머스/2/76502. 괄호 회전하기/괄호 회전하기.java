9d3e4621ff0f5ff2e15c45b8e43de34f751177ba
Angela <wpfkf4644@gmail.com>
2024-08-08 01:01:55 +0900

    [level 2] Title: 괄호 회전하기, Time: 26.91 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,32 +2,34 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        //1. 변수 묶어서 생성
-        String sb = s + s;
-        int answer = 0;
-        //2. isvalid 유효 확인하고
-        for(int i=0; i<s.length(); i++)
-        if(isValid(sb.substring(i,i + s.length()))) {
-            answer++;
-        }
-        return answer;   
-    }
-        //3. isvalid 규칙 짜기
-        public boolean isValid(String s) {
-            Deque<Character> stack =  new ArrayDeque<>();
-            
-            for(char cur : s.toCharArray()) {
-                if(cur=='(' || cur=='{' || cur=='[') {
-                    stack.push(cur);
-                } else {
-                    if(stack.isEmpty()) return false;
-                    char target = stack.pop();
-                    if(target == '(' && cur !=')' ||
-                       target == '{' && cur !='}' ||
-                       target == '[' && cur !=']'
-                      ) return false;
-                }
+        String sb = s+s;
+        int ans = 0;
+        
+        //유효확인
+        for(int i =0; i <s.length(); i++) {
+            if(isValid(sb.substring(i,s.length()+i))) {
+                ans++;
             }
-            return stack.isEmpty();
         }
+        return ans;
+    }
+        //유효구현
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for(char cur : s.toCharArray()) {
+            if(cur == '(' || cur == '{' || cur == '[') {
+                stack.push(cur);
+            } else {
+                if(stack.isEmpty()) return false;
+                
+                char target = stack.pop();
+                if(target == '(' && cur!= ')'|| 
+                   target == '{' && cur!= '}'|| 
+                   target == '['&& cur!= ']' )
+                    return false;
+            }
+        }
+            return stack.isEmpty();    
+    }
 }
