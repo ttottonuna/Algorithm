11e97ce788ba240ba012373d9dd093a178067f96
Angela <wpfkf4644@gmail.com>
2024-08-07 23:01:01 +0900

    [level 2] Title: 괄호 회전하기, Time: 34.64 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,32 +1,33 @@
-import java.util.Deque;
-import java.util.ArrayDeque;
+import java.util.*;
 
 class Solution {
- public int solution(String s) {
-     String sd = s + s ;
-     int ans = 0;
-     
-     for(int i =0; i < s.length(); i++) {
-         if (isValid(sd.substring(i,s.length()+i)))
-             ans++;
-     }
-     return ans;
- }
-    private boolean isValid(String s) {
-     Deque<String> stack = new ArrayDeque<>();
-        
-        for(char p : s.toCharArray()) {
-            if(p == '(') {
-                stack.push(")");
-            } else if (p == '{') {
-                stack.push("}");
-            } else if (p == '[') {
-                stack.push("]");
-            } else if (!stack.isEmpty() && stack.peek().equals(String.valueOf(p))) {
-                stack.pop();
-            } else
-                return false;
+    public int solution(String s) {
+        //1. 변수 묶어서 생성
+        String sb = s + s;
+        int answer = 0;
+        //2. isvalid 유효 확인하고
+        for(int i=0; i<s.length(); i++)
+        if(isValid(sb.substring(i,i + s.length()))) {
+            answer++;
+        }
+        return answer;   
     }
-        return stack.isEmpty();
-}
+        //3. isvalid 규칙 짜기
+        public boolean isValid(String s) {
+            Deque<Character> stack =  new ArrayDeque<>();
+            
+            for(char cur : s.toCharArray()) {
+                if(cur=='(' || cur=='{' || cur=='[') {
+                    stack.push(cur);
+                } else {
+                    if(stack.isEmpty()) return false;
+                    char target = stack.pop();
+                    if(target == '(' && cur !=')' ||
+                       target == '{' && cur !='}' ||
+                       target == '[' && cur !=']'
+                      ) return false;
+                }
+            }
+            return stack.isEmpty();
+        }
 }
