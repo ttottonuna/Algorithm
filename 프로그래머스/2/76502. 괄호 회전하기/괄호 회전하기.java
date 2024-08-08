5871450c150fcf51585592e2cfa43bf72401d589
Angela <wpfkf4644@gmail.com>
2024-08-09 00:08:10 +0900

    [level 2] Title: 괄호 회전하기, Time: 24.06 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,34 +2,34 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        String sb = s+s;
+        //두배로 만들기, ans 선언, 스택은 나중에 만들기
         int ans = 0;
-        
-        //유효확인
-        for(int i =0; i <s.length(); i++) {
-            if(isValid(sb.substring(i,s.length()+i))) {
+        String sb = s+s;
+    
+        // 유효확인 괄호가 유효하면 ans++ 리턴해라
+        for(int i=0; i<s.length(); i++) {
+            if(isValid(sb.substring(i,i+s.length()))) {
                 ans++;
             }
         }
         return ans;
     }
-        //유효구현
+        // 유효구현 스택 만들고 열린괄호 스택에 넣고, 스택에서 빼면서 닫힌 괄호랑 비교 
     public boolean isValid(String s) {
         Deque<Character> stack = new ArrayDeque<>();
         
         for(char cur : s.toCharArray()) {
-            if(cur == '(' || cur == '{' || cur == '[') {
+            if(cur=='(' || cur=='{' || cur=='[') {
                 stack.push(cur);
             } else {
                 if(stack.isEmpty()) return false;
-                
                 char target = stack.pop();
-                if(target == '(' && cur!= ')'|| 
-                   target == '{' && cur!= '}'|| 
-                   target == '['&& cur!= ']' )
-                    return false;
-            }
+                if(target =='(' && cur !=')' || 
+                   target =='{' && cur !='}' ||
+                   target =='[' && cur !=']'  ) 
+                 return false;   
         }
-            return stack.isEmpty();    
+        }
+        return stack.isEmpty();
     }
 }
